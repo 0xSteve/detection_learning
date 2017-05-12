dc8f014e63f3fc3375b0d99133f26d02332ee9c2
0xSteve <steven.porretta@gmail.com>
2017-05-12 03:51:18 -0400

    working 100% to expectation.

--- one_sensor_stationary/simulation.py
+++ one_sensor_stationary/simulation.py
@@ -6,20 +6,20 @@ import numpy as np
 import helpers as h
 import math
 
-num_actions = 5
+num_actions = 6
 
 env = Environment(num_actions)
 dlri = DLRI(num_actions)
 bestdepth = np.zeros(num_actions)
-E = [0.1, 0.2, 0.3, 0.3, 0.1]
-r = Pinger(E)
+E = [0.1, 0.2, 0.4, 0.2, 0.01, 0.09]
+det_obj = Pinger(E)
 for k in range(5):
     for j in range(100):
         # Caught me again...
         dlri.p = np.array(h.make_dp(num_actions))
         m = math.floor(num_actions / 2)
         while(True):
-            req = r.request()
+            req = det_obj.request()
             resp = env.response(m, req)
             if(not resp):
                 dlri.do_reward(m)
@@ -31,5 +31,5 @@ for k in range(5):
                 bestdepth[np.argmax(dlri.p)] += 1
                 break
     # print("The best depth tally is : " + str(bestdepth))
-    # print("Converge on depth: " + str(np.argmax(bestdepth)))
+    print("Converge on depth: " + str(np.argmax(bestdepth)))
     print("The probability vector is: " + str(bestdepth / sum(bestdepth)))
