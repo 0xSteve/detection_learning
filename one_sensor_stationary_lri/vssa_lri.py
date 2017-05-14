c7f6c76fe2b28b5d779ebf7ad8f5d960fa683709
0xSteve <steven.porretta@gmail.com>
2017-05-14 17:25:09 -0400

    Added more meaningful comments to the LRI automata definition.

--- one_sensor_stationary_lri/vssa_lri.py
+++ one_sensor_stationary_lri/vssa_lri.py
@@ -13,21 +13,34 @@ class Linear_R(object):
 
     def __init__(self, num_actions):
         '''Create a new Linear Reward-Inaction object.'''
+        # self.p is the learned action probability vector.
         self.p = np.array(h.make_p(num_actions))
+        # self.k = 1-lambda of the reward function for a LRI automata.
         self.k = 0.9
         self.n = 0
         self.best = 2 * num_actions  # Best time-cost.
 
     def next_action(self):
+        '''Pick the next action of the learning automata based on the
+           probability vector, self.p, of the LRI automata. At the first
+           time instant all action probabilities are equally likely.'''
+        # Pick a uniformly distributed random number to be tested against
+        # the CDF of self.p.  Used to determine the action of the
+        # automaton.
         randy = uniform(0, 1)  # Throwback to Archer.
+        # On catastrophic failure pick the first action.
         index = 0  # Worst case select the first action.
-        # print("The p is: " + str(self.p))
+        # print("The p is: " + str(self.p))  # Debug the change in self.p.
         cdf = h.cdf(self.p)
-        # print("The cdf is: " + str(cdf))
+        # print("The cdf is: " + str(cdf))  # Debug the selected action.
+        # index is the index of the CDF corresponding to the random
+        # action. This value is the next action the automaton will choose.
         index = h.get_index(randy, cdf)
         return index
 
     def do_reward(self, action):
+        '''Update the action probability, self.p, given the environment
+           issued a reward.'''
         self.p[action] += self.k * (1 - self.p[action])
         # Need to update penalty probabilities.
         for i in range(len(self.p)):
@@ -36,4 +49,5 @@ class Linear_R(object):
                 self.p[i] = (1 - self.k) * self.p[i]
 
     def do_penalty(self):
+        '''LRI automata do nothing on penalty.'''
         pass
