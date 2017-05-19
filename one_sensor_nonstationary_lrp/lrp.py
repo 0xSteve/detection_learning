35847617ea51e4cc3038bdb56097fb3e3f4c8686
0xSteve <steven.porretta@gmail.com>
2017-05-19 15:15:29 -0400

    Tuned the LRP to perform better in the new environments.

--- one_sensor_nonstationary_lrp/lrp.py
+++ one_sensor_nonstationary_lrp/lrp.py
@@ -16,7 +16,7 @@ class Linear_Reward_Penalty(object):
         # self.p is the learned action probability vector.
         self.p = np.array(h.make_p(num_actions))
         # self.a
-        self.a = 0.99
+        self.a = 0.8
         # sekf.b
         self.b = 0.5
         self.r = num_actions
