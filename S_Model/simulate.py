a5a6f75ed3f97703d639679de3a9ed59480b9870
0xSteve <steven.porretta@gmail.com>
2017-08-21 16:58:04 -0400

    Smodel complete with MSE

--- S_Model/simulate.py
+++ S_Model/simulate.py
@@ -4,18 +4,25 @@ from experiment import Experiment
 from random import uniform as u
 import numpy as np
 # First, we must read in the probability vector.
-f = open('unimodalNormalVector.csv', 'r')
+f = open('bimodalNormalVector.csv', 'r')
 p_vec = []
 reader = csv.reader(f)
 for row in reader:
     p_vec.append(float(row[0]))
 p_vec.pop()
+f = open('otherNormalVector.csv', 'r')
+p_vec2 = []
+reader = csv.reader(f)
+for row in reader:
+    p_vec2.append(float(row[0]))
+p_vec2.pop()
+p_vec = [p_vec, p_vec2]
 rando = int(u(0, 70))  # This will be the initial depth of the mobile-agent.
 # Create a new experiment with a randomly seeded depth for the agent and
 # precision of 1m, max depth of 70m.
-experiment = Experiment(rando, 70, p_vec, 10)
+experiment = Experiment(rando, 70, p_vec, 10, True)
 
-experiment.ensemble_evaluation(20)
+experiment.ensemble_evaluation_ns(20)
 print("********************************************************************")
 a = np.array(experiment.learned_best)
 b = np.array(p_vec)
@@ -45,19 +52,39 @@ w.close()
 # w.write(str(experiment.dist_est[len(experiment.dist_est) - 1]))
 # w.close()
 
+# w = open('Action1Probability.csv', 'w')
+# for i in range(len(experiment.action1_p) - 1):
+#     w.write(str(experiment.action1_p[i]) + '\n')
+# w.write(str(experiment.action1_p[len(experiment.action1_p) - 1]))
+# w.close()
+# w = open('Action0Probability.csv', 'w')
+# for i in range(len(experiment.action0_p) - 1):
+#     w.write(str(experiment.action0_p[i]) + '\n')
+# w.write(str(experiment.action0_p[len(experiment.action0_p) - 1]))
+# w.close()
+# w = open('Action2Probability.csv', 'w')
+# for i in range(len(experiment.action2_p) - 1):
+#     w.write(str(experiment.action2_p[i]) + '\n')
+# w.write(str(experiment.action2_p[len(experiment.action2_p) - 1]))
+# w.close()
+# print("Simulation complete!")
+
 w = open('Action1Probability.csv', 'w')
-for i in range(len(experiment.action1_p) - 1):
-    w.write(str(experiment.action1_p[i]) + '\n')
-w.write(str(experiment.action1_p[len(experiment.action1_p) - 1]))
+for k in range(2):
+    for i in range(len(experiment.action1_p[k]) - 1):
+        w.write(str(experiment.action1_p[k][i]) + '\n')
+    w.write(str(experiment.action1_p[k][len(experiment.action1_p[k]) - 1]))
 w.close()
 w = open('Action0Probability.csv', 'w')
-for i in range(len(experiment.action0_p) - 1):
-    w.write(str(experiment.action0_p[i]) + '\n')
-w.write(str(experiment.action0_p[len(experiment.action0_p) - 1]))
+for k in range(2):
+    for i in range(len(experiment.action0_p[k]) - 1):
+        w.write(str(experiment.action0_p[k][i]) + '\n')
+    w.write(str(experiment.action0_p[k][len(experiment.action0_p[k]) - 1]))
 w.close()
 w = open('Action2Probability.csv', 'w')
-for i in range(len(experiment.action2_p) - 1):
-    w.write(str(experiment.action2_p[i]) + '\n')
-w.write(str(experiment.action2_p[len(experiment.action2_p) - 1]))
+for k in range(2):
+    for i in range(len(experiment.action2_p[k]) - 1):
+        w.write(str(experiment.action2_p[k][i]) + '\n')
+    w.write(str(experiment.action2_p[k][len(experiment.action2_p[k]) - 1]))
 w.close()
 print("Simulation complete!")
